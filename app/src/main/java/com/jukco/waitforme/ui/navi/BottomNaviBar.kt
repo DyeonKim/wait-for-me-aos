01ca0bd822ef5a6f8bb6c15a460e26831cc55ab0
DyeonKim <devdoyeonkim@gmail.com>
2023-12-23 16:18:19 +0900

    design: BottomNaviBar (Material 2 -> Material 3)

--- app/src/main/java/com/jukco/waitforme/ui/navi/BottomNaviBar.kt
+++ app/src/main/java/com/jukco/waitforme/ui/navi/BottomNaviBar.kt
@@ -1,63 +1,59 @@
 package com.jukco.waitforme.ui.navi
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.jukco.waitforme.ui.theme.Pink40
-import com.jukco.waitforme.ui.theme.Pink80
+import androidx.navigation.compose.rememberNavController
+
+val BOTTOM_NAVI_ROUTE = listOf<BottomNaviItem>(
+    BottomNaviItem.PopsManagement,
+    BottomNaviItem.PopsList,
+    BottomNaviItem.Bookmark,
+    BottomNaviItem.WaitInfo,
+    BottomNaviItem.MyInfo,
+)
 
 @Composable
 fun BottomNaviBar(navController: NavHostController) {
     val navBAckStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBAckStackEntry?.destination
 
-    val items = listOf<BottomNaviItem>(
-        BottomNaviItem.PopsManagement,
-        BottomNaviItem.PopsList,
-        BottomNaviItem.WaitInfo,
-        BottomNaviItem.Bookmark,
-        BottomNaviItem.MyInfo
-    )
-
     when (currentDestination?.route) {
-        BottomNaviItem.PopsManagement.route -> BottomNaviTool(navController, currentDestination, items)
-        BottomNaviItem.PopsList.route -> BottomNaviTool(navController, currentDestination, items)
-        BottomNaviItem.WaitInfo.route -> BottomNaviTool(navController, currentDestination, items)
-        BottomNaviItem.Bookmark.route -> BottomNaviTool(navController, currentDestination, items)
-        BottomNaviItem.MyInfo.route -> BottomNaviTool(navController, currentDestination, items)
+        BottomNaviItem.PopsManagement.route -> BottomNavi(navController, currentDestination)
+        BottomNaviItem.PopsList.route -> BottomNavi(navController, currentDestination)
+        BottomNaviItem.WaitInfo.route -> BottomNavi(navController, currentDestination)
+        BottomNaviItem.Bookmark.route -> BottomNavi(navController, currentDestination)
+        BottomNaviItem.MyInfo.route -> BottomNavi(navController, currentDestination)
         else -> Unit
     }
 }
 
 @Composable
-fun BottomNaviTool(
+fun BottomNavi(
     navController: NavHostController,
     currentDestination: NavDestination?,
-    items: List<BottomNaviItem>
 ) {
-    BottomNavigation (
-        backgroundColor = Pink80,
-        modifier = Modifier
-            .fillMaxWidth()
-    ){
-        items.forEach { naviItem ->
-            BottomNavigationItem(
-                selected = currentDestination?.hierarchy?.any { it.route == naviItem.route } == true,
+    NavigationBar(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        BOTTOM_NAVI_ROUTE.forEach { bottomNaviItem ->
+            NavigationBarItem(
+                selected = currentDestination?.hierarchy?.any { it.route == bottomNaviItem.route } == true,
                 onClick = {
-                    navController.navigate(naviItem.route) {
+                    navController.navigate(bottomNaviItem.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
                         }
@@ -65,17 +61,29 @@ fun BottomNaviTool(
                         restoreState = true
                     }
                 },
-                selectedContentColor = Pink40,
-                unselectedContentColor = Color.Gray,
-                icon = { Icon(naviItem.icon, contentDescription = null) },
+                icon = {
+                    Icon(
+                        imageVector = bottomNaviItem.icon,
+                        contentDescription = null,
+                    )
+                },
                 label = {
                     Text(
-                        text = stringResource(naviItem.title),
+                        text = stringResource(bottomNaviItem.title),
                         style = MaterialTheme.typography.labelMedium,
-                        softWrap = false
+                        softWrap = false,
                     )
-                }
+                },
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun BottomNaviBarPreview() {
+    BottomNavi(
+        navController = rememberNavController(),
+        currentDestination = null,
+    )
 }
