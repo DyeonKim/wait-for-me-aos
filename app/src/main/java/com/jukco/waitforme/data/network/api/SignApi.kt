e15ad17c2faff00abd7ebae0957014ea3973d0b4
DyeonKim <devdoyeonkim@gmail.com>
2024-04-21 20:41:36 +0900

    feat: 회원가입 api 구현

--- app/src/main/java/com/jukco/waitforme/data/network/api/SignApi.kt
+++ app/src/main/java/com/jukco/waitforme/data/network/api/SignApi.kt
@@ -1,16 +1,24 @@
 package com.jukco.waitforme.data.network.api
 
 import com.jukco.waitforme.data.network.model.LocalSignInRequest
+import com.jukco.waitforme.data.network.model.LocalSignUpRequest
 import com.jukco.waitforme.data.network.model.SignInResponse
 import com.jukco.waitforme.data.network.model.SocialSignInRequest
+import com.jukco.waitforme.data.network.model.SocialSignUpRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SignApi {
     @POST("auth/local/sign-in")
-    suspend fun localSignIn(@Body request: LocalSignInRequest): Response<SignInResponse>
+    suspend fun localSignIn(@Body signInReq: LocalSignInRequest): Response<SignInResponse>
 
     @POST("auth/sns/sign-in")
-    suspend fun socialSignIn(@Body request: SocialSignInRequest): Response<SignInResponse>
+    suspend fun socialSignIn(@Body signInReq: SocialSignInRequest): Response<SignInResponse>
+
+    @POST("auth/local/sign-up")
+    suspend fun localSignUp(@Body signUpReq: LocalSignUpRequest): Response<SignInResponse>
+
+    @POST("auth/sns/sign-up")
+    suspend fun socialSignUp(@Body signUpReq: SocialSignUpRequest): Response<SignInResponse>
 }
--- app/src/test/java/com/jukco/waitforme/repository/SignRepositoryTest.kt
+++ app/src/test/java/com/jukco/waitforme/repository/SignRepositoryTest.kt
@@ -2,9 +2,12 @@ package com.jukco.waitforme.repository
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.jukco.waitforme.data.network.api.SignApi
+import com.jukco.waitforme.data.network.model.GenderType
 import com.jukco.waitforme.data.network.model.LocalSignInRequest
+import com.jukco.waitforme.data.network.model.LocalSignUpRequest
 import com.jukco.waitforme.data.network.model.Provider
 import com.jukco.waitforme.data.network.model.SocialSignInRequest
+import com.jukco.waitforme.data.network.model.SocialSignUpRequest
 import com.jukco.waitforme.data.repository.SignRepository
 import com.jukco.waitforme.data.repository.SignRepositoryImplementation
 import com.jukco.waitforme.fake.FakeDataSource
@@ -20,6 +23,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
+import java.net.HttpURLConnection
 
 class SignRepositoryTest {
     private lateinit var server: MockWebServer
@@ -47,7 +51,9 @@ class SignRepositoryTest {
     @Test
     fun signRepository_localSignIn_success() = runTest {
         val json = Json.encodeToJsonElement(FakeDataSource.signInResponse).toString()
-        val res = MockResponse().setBody(json)
+        val res = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(json)
         server.enqueue(res)
 
         val request = LocalSignInRequest("01012345678", "test123!")
@@ -55,13 +61,16 @@ class SignRepositoryTest {
         server.takeRequest()
 
         assertTrue(actualRes.isSuccessful)
+        assertEquals(HttpURLConnection.HTTP_OK, actualRes.code())
         assertEquals(FakeDataSource.signInResponse, actualRes.body())
     }
 
     @Test
     fun signRepository_socialSignIn_success() = runTest {
         val json = Json.encodeToJsonElement(FakeDataSource.signInResponse).toString()
-        val res = MockResponse().setBody(json)
+        val res = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(json)
         server.enqueue(res)
 
         val request = SocialSignInRequest(Provider.NAVER, "test123%23")
@@ -69,6 +78,54 @@ class SignRepositoryTest {
         server.takeRequest()
 
         assertTrue(actualRes.isSuccessful)
+        assertEquals(HttpURLConnection.HTTP_OK, actualRes.code())
+        assertEquals(FakeDataSource.signInResponse, actualRes.body())
+    }
+
+    @Test
+    fun signRepository_socialSignIn_noContent() = runTest {
+        val res = MockResponse().setResponseCode(HttpURLConnection.HTTP_NO_CONTENT)
+        server.enqueue(res)
+
+        val request = SocialSignInRequest(Provider.NAVER, "test123%23")
+        val actualRes = signRepository.socialSignIn(request)
+        server.takeRequest()
+
+        assertTrue(actualRes.isSuccessful)
+        assertEquals(HttpURLConnection.HTTP_NO_CONTENT, actualRes.code())
+    }
+
+    @Test
+    fun signRepository_localSignUp_success() = runTest {
+        val json = Json.encodeToJsonElement(FakeDataSource.signInResponse).toString()
+        val res = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(json)
+        server.enqueue(res)
+
+        val request = LocalSignUpRequest("01012345678", "", "test!123", false)
+        val actualRes = signRepository.localSignUp(request)
+        server.takeRequest()
+
+        assertTrue(actualRes.isSuccessful)
+        assertEquals(HttpURLConnection.HTTP_OK, actualRes.code())
+        assertEquals(FakeDataSource.signInResponse, actualRes.body())
+    }
+
+    @Test
+    fun signRepository_socialSignUp_success() = runTest {
+        val json = Json.encodeToJsonElement(FakeDataSource.signInResponse).toString()
+        val res = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(json)
+        server.enqueue(res)
+
+        val request = SocialSignUpRequest(Provider.NAVER, "test", "01012345678", false, "테스트입니다.", "", GenderType.OTHER, "")
+        val actualRes = signRepository.socialSignUp(request)
+        server.takeRequest()
+
+        assertTrue(actualRes.isSuccessful)
+        assertEquals(HttpURLConnection.HTTP_OK, actualRes.code())
         assertEquals(FakeDataSource.signInResponse, actualRes.body())
     }
 }
