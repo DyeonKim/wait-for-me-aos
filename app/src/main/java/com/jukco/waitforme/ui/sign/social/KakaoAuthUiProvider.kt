33d680fce4a7d8f4da90136ad1cfc9abff7a0fab
DyeonKim <devdoyeonkim@gmail.com>
2024-06-06 18:58:19 +0900

    refactor: 소셜로그인 코드 개선

--- app/src/main/java/com/jukco/waitforme/ui/sign/social/KakaoAuthUiProvider.kt
+++ app/src/main/java/com/jukco/waitforme/ui/sign/social/KakaoAuthUiProvider.kt
@@ -7,6 +7,9 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
@@ -14,23 +17,18 @@ class KakaoAuthUiProvider(
     private val activityContext: Context,
 ) : AuthUiProvider {
 
-    override suspend fun signIn(): SignUpDto? =
-        if (signInAndCheckSuccess()) {
-            getUserInfo()
-        } else {
-            null
-        }
-
-    private suspend fun signInAndCheckSuccess(): Boolean = suspendCancellableCoroutine { continuation ->
+    override suspend fun signIn(): SignUpDto? = suspendCancellableCoroutine { continuation ->
         // 카카오계정으로 로그인 공통 callback 구성
         // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 Log.e(TAG, "카카오계정으로 로그인 실패", error)
-                continuation.resume(false)
+                continuation.resume(null)
             } else if (token != null) {
                 Log.i(TAG, "카카오계정으로 로그인 성공")
-                continuation.resume(true)
+                CoroutineScope(Dispatchers.IO).launch {
+                    continuation.resume(getUserInfo())
+                }
             }
         }
 
@@ -43,7 +41,7 @@ class KakaoAuthUiProvider(
                     // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                     // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        continuation.resume(false)
+                        continuation.resume(null)
                         return@loginWithKakaoTalk
                     }
 
@@ -51,7 +49,9 @@ class KakaoAuthUiProvider(
                     UserApiClient.instance.loginWithKakaoAccount(activityContext, callback = callback)
                 } else if (token != null) {
                     Log.i(TAG, "카카오계정으로 로그인 성공")
-                    continuation.resume(true)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        continuation.resume(getUserInfo())
+                    }
                 }
             }
         } else {
